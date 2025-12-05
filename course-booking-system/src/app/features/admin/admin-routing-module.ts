d4df61b11973c4ef5645b12cb8b6ff25104edd70
Ahmed Davila Guerrero <c-aguerrero@kahua.com>
2025-12-05 19:08:13 -0300

    module

--- course-booking-system/src/app/features/admin/admin-routing-module.ts
+++ course-booking-system/src/app/features/admin/admin-routing-module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StudentListComponent } from './student-list/student-list';
+import { DashboardComponent } from './dashboard/dashboard';
+import { CreateCourseForm } from './create-course-form/create-course-form';
 
 const routes: Routes = [
   {
     path: '',
-    component: StudentListComponent,
+    component: DashboardComponent,
     children: [
       {
-        path: 'list-students',
-        loadComponent: () =>
-          import('./create-course-form/create-course-form').then((m) => m.CreateCourseForm),
+        path: 'students',
+        component: StudentListComponent,
+      },
+      {
+        path: 'new-course',
+        component: CreateCourseForm,
       },
     ],
   },
